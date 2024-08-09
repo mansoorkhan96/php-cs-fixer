5c1842bbe253b6da2ca0fa9d354c8f78669ebcd0
Mansoor Khan <8mansoorkhan@gmail.com>
2024-08-09 07:46:42 +0500

    format code

--- extension.js
+++ extension.js
@@ -1,156 +1,143 @@
-const vscode = require("vscode");
-const path = require("path");
-const fs = require("fs");
-const tmp = require("tmp");
-const cp = require("child_process");
+const vscode = require('vscode')
+const path = require('path')
+const fs = require('fs')
+const tmp = require('tmp')
+const cp = require('child_process')
 
 function formatDocument(document) {
-    if (document.languageId !== "php") {
-        return;
+    if (document.languageId !== 'php') {
+        return
     }
 
-    let toolPath = getConfig("toolPath");
-    let phpCmd = getConfig("phpCmd");
-    let filename = document.fileName;
-    let args = [];
+    let toolPath = getConfig('toolPath')
+    let phpCmd = getConfig('phpCmd')
+    let filename = document.fileName
+    let args = []
 
     let opts = {
         cwd: path.dirname(filename),
         shell: true,
-    };
+    }
 
-    if (getConfig("ignoreEnv")) {
+    if (getConfig('ignoreEnv')) {
         opts.env = { PHP_CS_FIXER_IGNORE_ENV: 1 }
     }
 
     if (getFixerFromComposer()) {
-        toolPath = getFixerFromComposer();
+        toolPath = getFixerFromComposer()
     }
 
     if (!toolPath) {
-        toolPath = path.resolve(__dirname, "php-cs-fixer");
+        toolPath = path.resolve(__dirname, 'php-cs-fixer')
     }
 
     if (!phpCmd) {
-        phpCmd = "php";
+        phpCmd = 'php'
     }
 
-    args.push(toolPath);
-    args.push("fix");
+    args.push(toolPath)
+    args.push('fix')
 
-    if (!getConfig("useCache")) {
-        args.push("--using-cache=no");
+    if (!getConfig('useCache')) {
+        args.push('--using-cache=no')
     }
 
-    if (getConfig("allowRisky")) {
-        args.push("--allow-risky=yes");
+    if (getConfig('allowRisky')) {
+        args.push('--allow-risky=yes')
     }
 
-    let config = getConfig("config");
+    let config = getConfig('config')
 
     if (getPreset()) {
-        args.push("--config=" + getPreset());
+        args.push('--config=' + getPreset())
     } else if (config) {
         // Support config file with relative path
         if (!path.isAbsolute(config)) {
-            let currentPath = opts.cwd;
-            let triedPaths = [currentPath];
+            let currentPath = opts.cwd
+            let triedPaths = [currentPath]
             while (!fs.existsSync(currentPath + path.sep + config)) {
-                let lastPath = currentPath;
-                currentPath = path.dirname(currentPath);
+                let lastPath = currentPath
+                currentPath = path.dirname(currentPath)
                 if (lastPath == currentPath) {
-                    vscode.window.showErrorMessage(
-                        `Unable to find ${config} file in ${triedPaths.join(", ")}`
-                    );
-                    return;
+                    vscode.window.showErrorMessage(`Unable to find ${config} file in ${triedPaths.join(', ')}`)
+                    return
                 } else {
-                    triedPaths.push(currentPath);
+                    triedPaths.push(currentPath)
                 }
             }
-            config = currentPath + path.sep + config;
+            config = currentPath + path.sep + config
         }
 
-        args.push("--config=" + config);
+        args.push('--config=' + config)
     } else if (projectConfigFile()) {
-        args.push("--config=" + projectConfigFile());
+        args.push('--config=' + projectConfigFile())
     } else {
-        let rules = getConfig("rules");
+        let rules = getConfig('rules')
         if (rules) {
-            args.push("--rules=" + rules);
+            args.push('--rules=' + rules)
         }
     }
 
-    const tmpFile = tmp.fileSync();
-    fs.writeFileSync(tmpFile.name, document.getText(null));
+    const tmpFile = tmp.fileSync()
+    fs.writeFileSync(tmpFile.name, document.getText(null))
 
     return new Promise(function (resolve) {
         cp.execFile(phpCmd, [...args, tmpFile.name], opts, function (err) {
             if (err) {
-                tmpFile.removeCallback();
+                tmpFile.removeCallback()
 
-                if (err.code === "ENOENT") {
-                    vscode.window.showErrorMessage(
-                        "Unable to find the php-cs-fixer tool."
-                    );
-                    throw err;
+                if (err.code === 'ENOENT') {
+                    vscode.window.showErrorMessage('Unable to find the php-cs-fixer tool.')
+                    throw err
                 }
 
                 vscode.window.showErrorMessage(
-                    "There was an error while running php-cs-fixer. Check the Developer Tools console for more information."
-                );
-                throw err;
+                    'There was an error while running php-cs-fixer. Check the Developer Tools console for more information.'
+                )
+                throw err
             }
 
-            const text = fs.readFileSync(tmpFile.name, "utf-8");
-            tmpFile.removeCallback();
+            const text = fs.readFileSync(tmpFile.name, 'utf-8')
+            tmpFile.removeCallback()
 
-            resolve(text);
-        });
-    });
+            resolve(text)
+        })
+    })
 }
 
 function getProjectRoot() {
-    return vscode.workspace.getWorkspaceFolder(
-        vscode.window.activeTextEditor.document.uri
-    ).uri.fsPath;
+    return vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri).uri.fsPath
 }
 
 function getFixerFromComposer() {
-    let composerPath =
-        getProjectRoot() +
-        path.sep +
-        "vendor" +
-        path.sep +
-        "bin" +
-        path.sep +
-        "php-cs-fixer";
+    let composerPath = getProjectRoot() + path.sep + 'vendor' + path.sep + 'bin' + path.sep + 'php-cs-fixer'
 
     if (fs.existsSync(composerPath)) {
-        return composerPath;
+        return composerPath
     }
 
-    return null;
+    return null
 }
 
 function projectConfigFile() {
-    if (fs.existsSync(getProjectRoot() + path.sep + ".php-cs-fixer.php")) {
-        return getProjectRoot() + path.sep + ".php-cs-fixer.php";
+    if (fs.existsSync(getProjectRoot() + path.sep + '.php-cs-fixer.php')) {
+        return getProjectRoot() + path.sep + '.php-cs-fixer.php'
     }
 
-    if (fs.existsSync(getProjectRoot() + path.sep + ".php-cs-fixer.dist.php")) {
-        return getProjectRoot() + path.sep + ".php-cs-fixer.dist.php";
+    if (fs.existsSync(getProjectRoot() + path.sep + '.php-cs-fixer.dist.php')) {
+        return getProjectRoot() + path.sep + '.php-cs-fixer.dist.php'
     }
 
-    return null;
+    return null
 }
 
 function getPreset() {
     const preset = getConfig('preset')
 
     if (['laravel', 'per', 'psr12', 'symfony'].includes(preset.toLowerCase())) {
-        return path.resolve(__dirname, `presets/${preset.toLowerCase()}.php`);
+        return path.resolve(__dirname, `presets/${preset.toLowerCase()}.php`)
     } else if (preset) {
-        vscode.window.showErrorMessage(`Unable to find preset: ${preset}.`);
+        vscode.window.showErrorMessage(`Unable to find preset: ${preset}.`)
     }
 }
 
@@ -161,50 +148,45 @@ function registerDocumentProvider(document, options) {
                 const range = new vscode.Range(
                     new vscode.Position(0, 0),
                     document.lineAt(document.lineCount - 1).range.end
-                );
-                resolve([new vscode.TextEdit(range, text)]);
+                )
+                resolve([new vscode.TextEdit(range, text)])
             })
             .catch(function (err) {
-                reject();
-            });
-    });
+                reject()
+            })
+    })
 }
 
 function getConfig(key) {
-    return vscode.workspace.getConfiguration("php-cs-fixer").get(key);
+    return vscode.workspace.getConfiguration('php-cs-fixer').get(key)
 }
 
 function activate(context) {
     context.subscriptions.push(
-        vscode.commands.registerTextEditorCommand(
-            "php-cs-fixer.fix",
-            function (textEditor) {
-                vscode.commands.executeCommand("editor.action.formatDocument");
-            }
-        )
-    );
+        vscode.commands.registerTextEditorCommand('php-cs-fixer.fix', function (textEditor) {
+            vscode.commands.executeCommand('editor.action.formatDocument')
+        })
+    )
 
     context.subscriptions.push(
         vscode.workspace.onWillSaveTextDocument(function (event) {
             if (event.document.languageId === 'php' && getConfig('fixOnSave')) {
-                event.waitUntil(
-                    vscode.commands.executeCommand("editor.action.formatDocument")
-                );
+                event.waitUntil(vscode.commands.executeCommand('editor.action.formatDocument'))
             }
         })
-    );
+    )
 
     context.subscriptions.push(
-        vscode.languages.registerDocumentFormattingEditProvider("php", {
+        vscode.languages.registerDocumentFormattingEditProvider('php', {
             provideDocumentFormattingEdits: function (document, options) {
-                return registerDocumentProvider(document, options);
+                return registerDocumentProvider(document, options)
             },
         })
-    );
+    )
 }
 
-exports.activate = activate;
+exports.activate = activate
 
-function deactivate() { }
+function deactivate() {}
 
-exports.deactivate = deactivate;
+exports.deactivate = deactivate
